f313e92e0d213985a4d237e9dfa31c6ccffd76c2
fan6 <fanchenyou@gmail.com>
2019-09-27 17:11:49 -0700

    Initial commit.

--- Part_0 _Q-Tables.py
+++ Part_0 _Q-Tables.py
@@ -12,10 +12,10 @@ env = gym.make('FrozenLake-v0')
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 # Set learning parameters
 lr = .8
-y = .95
+gammar = .95
 num_episodes = 2000
 # create lists to contain total rewards and steps per episode
-# jList = []
+
 rList = []
 for i in range(num_episodes):
     # Reset environment and get first new observation
@@ -32,12 +32,12 @@ for i in range(num_episodes):
         # Get new state and reward from environment
         s1, r, d, _ = env.step(a)
         # Update Q-Table with new knowledge
-        Q[s, a] = Q[s, a] + lr * (r + y * np.max(Q[s1, :]) - Q[s, a])
+        Q[s, a] = Q[s, a] + lr * (r + gammar * np.max(Q[s1, :]) - Q[s, a])
         rAll += r
         s = s1
         if d == True:
             break
-    # jList.append(j)
+
     rList.append(rAll)
 
 print("Score over time: " + str(sum(rList) / num_episodes))
